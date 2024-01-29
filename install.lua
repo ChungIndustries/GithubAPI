a5ce0f12bcec62daefc42f42e0bb446d7b6aa103
Christian <chrille_0313@hotmail.com>
2024-01-29 19:02:53 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -4,9 +4,13 @@ local REPO = "GithubAPI"
 local BRANCH = "master"
 local URL = BASE_URL .. "/" .. USER .. "/" .. REPO .. "/" .. BRANCH .. "/"
 
-local dependencies = {
+local api_dependencies = {
     "/src/apis/github.lua"
 }
+
+local function get_file_name(path)
+    return fs.getName(path)
+end
 
 local function get_content(url) 
     local response = http.get(url)
@@ -22,9 +26,11 @@ local function write_file(path, content)
 end
 
 local function main()
-    for _, dependency in pairs(dependencies) do
+    for _, dependency in pairs(api_dependencies) do
         local content = get_content(URL..dependency)
-        write_file(REPO..dependency, content)
+        local file_path = REPO .. "/" ..get_file_name(dependency)
+        write_file(file_path, content)
+        os.load(file_path)
     end
 end
 
